e486ec80b9f61f02a40cb8124750032e96c4e3c0
Kevin WANG <sirewk@hotmail.fr>
2026-03-26 15:39:50 +0100

    wip

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Attendre un peu que la DB soit prête (optionnel mais recommandé)
-sleep 10
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-    # Téléchargement de WordPress
     wp core download --allow-root
 
-    # Création du fichier wp-config.php
     wp config create --allow-root \
         --dbname=$MYSQL_DATABASE \
         --dbuser=$MYSQL_USER \
         --dbpass=$MYSQL_PASSWORD \
         --dbhost=mariadb:3306
 
-    # Installation finale (Admin, mot de passe, email)
     wp core install --allow-root \
         --url=$DOMAIN_NAME \
         --title=$WP_TITLE \
@@ -23,8 +18,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
         --admin_email=$WP_ADMIN_EMAIL
 fi
 
-# Création du dossier nécessaire pour PHP-FPM
 mkdir -p /run/php
 
-# Lancement de PHP-FPM en arrière-plan
 exec /usr/sbin/php-fpm7.4 -F
