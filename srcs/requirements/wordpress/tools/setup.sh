52b5578b577fdfdb0332589dea0a25540c297aeb
Kevin WANG <sirewk@hotmail.fr>
2026-04-04 12:29:36 +0200

    fix(setup): streamline WordPress setup process and ensure MariaDB readiness

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
 set -e
 
-
-if [ ! -f /var/www/html/index.php ]; then
-	echo "WordPress: Downloading and setting up WordPress files"
-    tmp_dir="$(mktemp -d)"
-    curl -fsSL https://wordpress.org/latest.tar.gz -o "$tmp_dir/wordpress.tar.gz"
-    tar -xzf "$tmp_dir/wordpress.tar.gz" -C "$tmp_dir"
-    cp -a "$tmp_dir/wordpress/." /var/www/html/
-    rm -rf "$tmp_dir"
-fi
+echo "WordPress: Waiting for MariaDB..."
+until mariadb-admin ping -h"mariadb" -P"3306" -u"$WORDPRESS_DB_USER" -p"$(cat /run/secrets/wp_password)" > /dev/null 2>&1; do
+	sleep 2
+done
+echo "WordPress: MariaDB is ready"
 
 echo "WordPress: Configuring WordPress"
-wp core download --locale=fr_FR --allow-root --force
-
-# if [ ! -f wp-config.php ]; then
+if [ ! -f /var/www/html/wp-config.php ]; then
+	wp core download --allow-root --path=/var/www/html
 	echo "WordPress: Creating wp-config.php"
 	wp config create --allow-root \
-    --dbname="$WORDPRESS_DB_NAME" \
-    --dbuser="$WORDPRESS_DB_USER" \
-    --dbpass="$(cat /run/secrets/wp_password)" \
-    --dbhost="$WORDPRESS_DB_HOST" \
-    --force
-# fi
-
-# if [ ! wp core is-installed --allow-root ]; then
-	echo "WordPress: Configuring WordPress"
-	wp core install --url=$DOMAIN_NAME \
-					--title=$SITE_TITLE \
-					--admin_user=$ADMIN_USER \
-					--admin_password=$(cat /run/secrets/user_password) \
-					--admin_email=$ADMIN_EMAIL \
-					--locale=fr_FR \
-					--allow-root
-# fi
-
-if ! wp user get $JOJO_USER --allow-root > /dev/null 2>&1; then
-	echo "WordPress: Creating user"
-	wp user create $JOJO_USER $JOJO_EMAIL --user_pass=$(cat /run/secrets/jojo_password) --role=author --allow-root
+		--skip-check \
+		--dbname="$WORDPRESS_DB_NAME" \
+		--dbuser="$WORDPRESS_DB_USER" \
+		--dbpass="$(cat /run/secrets/wp_password)" \
+		--dbhost="$WORDPRESS_DB_HOST"
 fi
 
-chmod -R 755 /var/www/html
-chown -R www-data:www-data /var/www/html
+
+if ! wp core is-installed --allow-root --path=/var/www/html > /dev/null 2>&1; then
+	echo "WordPress: Installing WordPress"
+	wp core install --path=/var/www/html --url="$DOMAIN_NAME" \
+					--title="$SITE_TITLE" \
+					--admin_user="$ADMIN_USER" \
+					--admin_password="$(cat /run/secrets/user_password)" \
+					--admin_email="$ADMIN_EMAIL" \
+					--locale=fr_FR \
+					--allow-root
+fi
+
+if ! wp user get "$JOJO_USER" --allow-root --path=/var/www/html > /dev/null 2>&1; then
+	echo "WordPress: Creating user"
+	wp user create "$JOJO_USER" "$JOJO_EMAIL" --user_pass="$(cat /run/secrets/jojo_password)" --role=author --allow-root --path=/var/www/html
+fi
+
 mkdir -p /run/php
 
 echo "WordPress: Setup complete, starting PHP-FPM"
